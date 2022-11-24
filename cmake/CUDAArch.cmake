c12c180c71200e16026b1ca192c0c6174c8033be
Peter Heywood <peethwd@gmail.com>
2022-11-24 01:24:10 +0000

    validate user provided + generate default all-major set for older cmake via nvcc --help

--- cmake/CUDAArch.cmake
+++ cmake/CUDAArch.cmake
@@ -13,15 +13,29 @@ endfunction()
 
 # @todo - make validation default but optional
 function(ccad_apply)
-    # message("apply ${arch_from_env_or_cache}")
-
-    # @todo - setting the languge to CUDA sets the dfefault, but doesn't validate the user provided a sane option otherwise. We should validate that here rather than waiting till the first all to nvcc, and optionally set it to the default? (probably not)
-    # CMake 3.24 doesnt' vlaidate all/all-major properpyl, so if its one of the 3 knwon versions we should block it being a list too.
+    find_package(CUDAToolkit REQUIRED)
+    # Query NVCC for the acceptable SM values, this is used in multiple places
+    if(NOT DEFINED SUPPORTED_CUDA_ARCHITECTURES_NVCC)
+        execute_process(COMMAND ${CUDAToolkit_NVCC_EXECUTABLE} "--help" OUTPUT_VARIABLE NVCC_HELP_STR ERROR_VARIABLE NVCC_HELP_STR)
+        # Match all comptue_XX or sm_XXs
+        string(REGEX MATCHALL "'(sm|compute)_[0-9]+'" SUPPORTED_CUDA_ARCHITECTURES_NVCC "${NVCC_HELP_STR}" )
+        # Strip just the numeric component
+        string(REGEX REPLACE "'(sm|compute)_([0-9]+)'" "\\2" SUPPORTED_CUDA_ARCHITECTURES_NVCC "${SUPPORTED_CUDA_ARCHITECTURES_NVCC}" )
+        # Remove dupes and sort to build the correct list of supported CUDA_ARCH.
+        list(REMOVE_DUPLICATES SUPPORTED_CUDA_ARCHITECTURES_NVCC)
+        list(REMOVE_ITEM SUPPORTED_CUDA_ARCHITECTURES_NVCC "")
+        list(SORT SUPPORTED_CUDA_ARCHITECTURES_NVCC)
+        # Store the supported arch's once and only once. This could be a cache  var given the cuda compiler should not be able to change without clearing th cache?
+        set(SUPPORTED_CUDA_ARCHITECTURES_NVCC ${SUPPORTED_CUDA_ARCHITECTURES_NVCC} PARENT_SCOPE)
+    endif()
+    list(LENGTH SUPPORTED_CUDA_ARCHITECTURES_NVCC SUPPORTED_CUDA_ARCHITECTURES_NVCC_COUNT)
 
     # If we already have a cuda architetures value, validate it as CMake doesn't.
     if(arch_from_env_or_cache AND NOT CMAKE_CUDA_ARCHITECTURES STREQUAL "")
         # Get the number or architectures specified
         list(LENGTH CMAKE_CUDA_ARCHITECTURES arch_count)
+        # Prep a bool to track if a single special value is being used or not
+        set(using_keyword_arch FALSE)
         # native requires CMake >= 3.24, and must be the only option.
         if("native" IN_LIST CMAKE_CUDA_ARCHITECTURES)
             # Error if CMake is too old
@@ -36,6 +50,7 @@ function(ccad_apply)
                     " CMAKE_CUDA_ARCHITECTURES value `native` must be the only value specified.\n"
                     " CMAKE_CUDA_ARCHITECTURES=\"${CMAKE_CUDA_ARCHITECTURES}\"")
             endif()
+            set(using_keyword_arch TRUE)
         endif()
         # all requires 3.23, and must be the sole value.
         if("all" IN_LIST CMAKE_CUDA_ARCHITECTURES)
@@ -51,6 +66,7 @@ function(ccad_apply)
                     " CMAKE_CUDA_ARCHITECTURES value `all` must be the only value specified.\n"
                     " CMAKE_CUDA_ARCHITECTURES=\"${CMAKE_CUDA_ARCHITECTURES}\"")
             endif()
+            set(using_keyword_arch TRUE)
         endif()
         # all-major requires 3.23, and must be the sole value.
         if("all-major" IN_LIST CMAKE_CUDA_ARCHITECTURES)
@@ -66,36 +82,27 @@ function(ccad_apply)
                     " CMAKE_CUDA_ARCHITECTURES value `all-major` must be the only value specified.\n"
                     " CMAKE_CUDA_ARCHITECTURES=\"${CMAKE_CUDA_ARCHITECTURES}\"")
             endif()
+            set(using_keyword_arch TRUE)
         endif()
 
         # Cmake 3.18+ expects a list of 1 or more <sm>, <sm>-real or <sm>-virtual.
-        # CMake isn't aware of the exact SMS supported by the CUDA version afiak, but we can query nvcc --help and extract the output. Only query this once.
-        if(NOT DEFINED SUPPORTED_CUDA_ARCHITECTURES_NVCC)
-            execute_process(COMMAND ${CMAKE_CUDA_COMPILER} "--help" OUTPUT_VARIABLE NVCC_HELP_STR ERROR_VARIABLE NVCC_HELP_STR)
-            # Match all comptue_XX or sm_XXs
-            string(REGEX MATCHALL "'(sm|compute)_[0-9]+'" SUPPORTED_CUDA_ARCHITECTURES_NVCC "${NVCC_HELP_STR}" )
-            # Strip just the numeric component
-            string(REGEX REPLACE "'(sm|compute)_([0-9]+)'" "\\2" SUPPORTED_CUDA_ARCHITECTURES_NVCC "${SUPPORTED_CUDA_ARCHITECTURES_NVCC}" )
-            # Remove dupes and sort to build the correct list of supported CUDA_ARCH.
-            list(REMOVE_DUPLICATES SUPPORTED_CUDA_ARCHITECTURES_NVCC)
-            list(REMOVE_ITEM SUPPORTED_CUDA_ARCHITECTURES_NVCC "")
-            list(SORT SUPPORTED_CUDA_ARCHITECTURES_NVCC)
-            # Store the supported arch's once and only once. This could be a cache  var given the cuda compiler should not be able to change without clearing th cache?
-            set(SUPPORTED_CUDA_ARCHITECTURES_NVCC ${SUPPORTED_CUDA_ARCHITECTURES_NVCC} PARENT_SCOPE)
+        # CMake isn't aware of the exact SMS supported by the CUDA version afiak, but we have already queired nvcc for this (once and only once)
+        # If nvcc parsing worked and a single keyword option is not being used, attempt the validation:
+        if(SUPPORTED_CUDA_ARCHITECTURES_NVCC_COUNT GREATER 0 AND NOT using_keyword_arch)
+            # Transform a copy of the list of supported architectures, to hopefully just contain numbers
+            set(archs ${CMAKE_CUDA_ARCHITECTURES})
+            list(TRANSFORM archs REPLACE "(\-real|\-virtual)" "")
+            # If any of the specified architectures are not in the nvcc reported list, error.
+            foreach(ARCH IN LISTS archs)
+                if(NOT ARCH IN_LIST SUPPORTED_CUDA_ARCHITECTURES_NVCC)
+                    message(FATAL_ERROR
+                        " CMAKE_CUDA_ARCHITECTURES value `${ARCH}` is not supported by nvcc ${CMAKE_CUDA_COMPILER_VERSION}.\n"
+                        " Supported architectures based on nvcc --help: \n"
+                        "   ${SUPPORTED_CUDA_ARCHITECTURES_NVCC}\n")
+                endif()
+            endforeach()
+            unset(archs)
         endif()
-        # Iterate the options, checking each is allowed based on the nvcc help output, erroring if not.
-        # First transform the list removing -real/-virtual, to simplify logic within the loop.
-        set(archs ${CMAKE_CUDA_ARCHITECTURES})
-        list(TRANSFORM archs REPLACE "(\-real|\-virtual)" "")
-        foreach(ARCH IN LISTS archs)
-            if(NOT ARCH IN_LIST SUPPORTED_CUDA_ARCHITECTURES_NVCC)
-                message(FATAL_ERROR
-                    " CMAKE_CUDA_ARCHITECTURES value `${ARCH}` is not supported by nvcc ${CMAKE_CUDA_COMPILER_VERSION}.\n"
-                    " Supported architectures based on nvcc --help: \n"
-                    "   ${SUPPORTED_CUDA_ARCHITECTURES_NVCC}\n")
-            endif()
-        endforeach()
-        unset(archs)
         # If no errors yet, we're good to go.
         return()
     endif()
@@ -103,14 +110,28 @@ function(ccad_apply)
     # If we're using CMake >= 3.23, we can just use all-major, though we then have to find the minimum a different way?
     if(CMAKE_VERSION VERSION_GREATER_EQUAL "3.23")
         set(CMAKE_CUDA_ARCHITECTURES "all-major" PARENT_SCOPE)
-
     else()
         # For CMake < 3.23, we have to make our own all-major equivalent.
-        # @todo - could we query nvcc here to find all architectures, then pull the major ones plus oldest arch?
-        set(default_archs "35;50;60;70;80")
-        # @todo - rather than building a list up, we could start with an ideal list, then remove 90 if not supported via nvcc output?
-        if(CMAKE_CUDA_COMPILER_VERISON VERSION_GREATER_EQUAL 11.8.0)
-            list(APPEND "90")
+        # If we have nvcc help outut, we can generate this from all the elements that end with a 0 (and the first element if it does not.)
+        if(SUPPORTED_CUDA_ARCHITECTURES_NVCC_COUNT GREATER 0)
+            # If the lowest support arch is not major, add it to the default
+            list(GET SUPPORTED_CUDA_ARCHITECTURES_NVCC 0 lowest_supported)
+            if(NOT lowest_supported MATCHES "0$")
+                list(APPEND default_archs ${lowest_supported})
+            endif()
+            unset(lowest_supported)
+            # For each architecture, if it is major add it to the default list
+            foreach(ARCH IN LISTS SUPPORTED_CUDA_ARCHITECTURES_NVCC)
+                if(ARCH MATCHES "0$")
+                    list(APPEND default_archs ${ARCH})
+                endif()
+            endforeach()
+        else()
+            # If nvcc help output parsing failed, just use an informed guess option from CUDA 11.8
+            set(default_archs "35;50;60;70;80")
+            if(CMAKE_CUDA_COMPILER_VERSION VERSION_GREATER_EQUAL 11.8)
+                list(APPEND default_archs "90")
+            endif()
         endif()
         # We actually want real for each arch, then virtual for the final, but only for library-provided values, to only embed one arch worth of ptx.
         # So grab the last element of the list
